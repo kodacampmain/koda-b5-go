95ab3d7270184048c484fad017680ba181978e75
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-12-24 09:34:19 +0700

    feat: adds bufio for user input

---
+++ internals/userinput/run.go
@@ -0,0 +1,27 @@
+package userinput
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Init() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Masukkan Pilihan: ")
+		scanner.Scan()
+		input := scanner.Text()
+		if input == "exit" {
+			fmt.Println("Selamat Tinggal")
+			break
+		}
+		if input == "1" {
+			fmt.Print("Masukkan Perintah: ")
+			scanner.Scan()
+			input := scanner.Text()
+			fmt.Printf("Perintah diterima <=== %s\n", input)
+		}
+		fmt.Printf("Input => %s\n", input)
+	}
+}
--- main.go
+++ main.go
@@ -1,32 +1,27 @@
 package main
 
 import (
-	"fmt"
-
-	arrayslice "github.com/kodacampmain/koda-b5-go/internals/arraySlice"
-	cdn "github.com/kodacampmain/koda-b5-go/internals/conditionals"
-	l "github.com/kodacampmain/koda-b5-go/internals/loop"
-	"github.com/kodacampmain/koda-b5-go/internals/pokemons"
+	"github.com/kodacampmain/koda-b5-go/internals/userinput"
 )
 
 func main() {
 	// log.Println("Hello World")
 	// fmt.Println([]byte("Hello World"))
 	// fmt.Println([]rune("ᄈ"))
-	var myName string // Manifest
-	age := -1         // Inference
-	myName = "Koda"   // Assignment
+	// var myName string // Manifest
+	// age := -1         // Inference
+	// myName = "Koda"   // Assignment
 	// fmt.Println(myName)
 	// fmt.Println(age)
 	// fmt.Println(reflect.TypeOf(age))
-	cdn.DetermineOldOrYoung(age)
+	// cdn.DetermineOldOrYoung(age)
 	// greeting, error := CreateGreetingWithNameAndAge(myName, age)
 	// var greeting string
-	if greeting, error := cdn.CreateGreetingWithNameAndAge(myName, age); error != nil {
-		fmt.Println(error.Error())
-	} else {
-		fmt.Printf("My Greeting\n%s\n", greeting)
-	}
+	// if greeting, error := cdn.CreateGreetingWithNameAndAge(myName, age); error != nil {
+	// 	fmt.Println(error.Error())
+	// } else {
+	// 	fmt.Printf("My Greeting\n%s\n", greeting)
+	// }
 	// fmt.Println(greeting)
 	// konversi tipe data = tipe(variabel)
 	// fmt.Println(float32(age), reflect.TypeOf(float32(age)))
@@ -34,8 +29,9 @@ func main() {
 	// ppnStr := strconv.Itoa(int(ppn))
 	// fmt.Println(ppn, ppnStr)
 	// fmt.Println(string(ppn))
-	l.PrintEvenNumber(10)
-	arrayslice.ArrayAndSlice()
+	// l.PrintEvenNumber(10)
+	// arrayslice.ArrayAndSlice()
 
-	pokemons.GetPokemon()
+	// pokemons.GetPokemon()
+	userinput.Init()
 }
